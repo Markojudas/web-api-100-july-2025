87c5fc720d7fb1331210c396469c98881fdb730e
Jose R Hernandez <jose@markojudas.com>
2025-07-30 13:35:58 +0000

    Wednesday - start of day

--- src/SoftwareCenterSolution/SoftwareCenter.Api/Program.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Program.cs
@@ -1,22 +1,34 @@
+using FluentValidation;
 using Marten;
+using SoftwareCenter.Api.Vendors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("db") ??
+
+var connectionString = builder.Configuration.GetConnectionString("db") ?? 
     throw new Exception("Need a connection string");
 
+Console.WriteLine("Using Connection String: " + connectionString);
 builder.Services.AddMarten(config =>
 {
     config.Connection(connectionString);
+
 }).UseLightweightSessions();
+
+builder.Services.AddScoped<IValidator<CreateVendorRequest>, CreateVendorRequestValidator>();
+builder.Services.AddScoped<IValidator<CreateVendorPointOfContactRequest>, CreateVendorPointOfContactRequestValidator>();
+// it will give us a scoped service called IDocumentSession
+// if this was Entity framework, it would give us a "DbContext" object we can use.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
+// everything after this is configuring "middleware"
+// that is is stuff that will intercept incoming our outgoing HTTP requests
+// and process them in some way.
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -25,9 +37,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.MapControllers(); // Go Find all the controllers and look at the attributes (HttPGet, HttpPost, etc...)
-Console.WriteLine("Fiaxing to run your API");
-app.Run();
-Console.WriteLine("Done");
+app.MapControllers(); // Go find all the controllers and look at the attributes (HttpGet, HttpPost, etc.)
+// and make yourself a cheat sheet.
+// if I get a POST /vendors - create a Vendors/Controller instance, and call the AddVendorAsync Method.
+Console.WriteLine("Fixing to run your API");
+app.Run(); // this is a "blocking method" basically a while(true) {... }
+Console.WriteLine("done running your API");
 
-public partial class Program;
+public partial class Program; // In .NET 10 you won't have to do this.
--- src/SoftwareCenterSolution/SoftwareCenter.Api/Vendors/Controller.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Vendors/Controller.cs
@@ -1,133 +1,128 @@
-﻿using Marten;
-
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using Marten;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SoftwareCenter.Api.Vendors;
 
-public class Controller(IDocumentSession session) : ControllerBase
 
+public class Controller(IDocumentSession session) : ControllerBase
 {
 
-
-
-
-
     // this is the method you should call when a POST /vendors is received.
-
     [HttpPost("/vendors")]
-
     public async Task<ActionResult> AddAVendorAsync(
-
         [FromBody] CreateVendorRequest request,
-
+        [FromServices] IValidator<CreateVendorRequest> validator,
         CancellationToken token)
-
     {
+        //if(!ModelState.IsValid)
+        // {
+        //     return BadRequest(ModelState);
+        // }
 
-
+        var validationResults = await validator.ValidateAsync(request);
+        if(!validationResults.IsValid)
+        {
+            return BadRequest(validationResults);
+        }
         // validation
-
         // You can't add a vendor with the same name more than once.
-
         // field validation - what is required, what is optional, what are the rules for the required things
-
         // domain validation - we don't already have a vendor with that same name
-
         // 
-
         // we have to "save it" somewhere. 
-
+       
         // Mapping Code (copy from one object to another)
-
         var response = new CreateVendorResponse(
-
             Guid.NewGuid(),
-
             request.Name,
-
             request.Url,
-
             request.PointOfContact
-
             );
-
         session.Store(response);
-
         await session.SaveChangesAsync();
-
-        return Ok(response);
-
+        return Ok(response); 
     }
 
 
     // GET /vendors/tacos
-
     [HttpGet("/vendors/{id:guid}")]
-
     public async Task<ActionResult> GetVendorByIdAsync(Guid id, CancellationToken token)
-
     {
-
         // look that thing up in the database.
-
         var response = await session
-
             .Query<CreateVendorResponse>()
-
             .Where(v => v.Id == id)
-
             .SingleOrDefaultAsync();
 
         if (response is null)
-
         {
-
             return NotFound();
-
         }
-
         else
-
         {
-
             return Ok(response);
-
         }
+    }
 
+    [HttpGet("/vendors")]
+    public async Task<ActionResult> GetAllVendorsAsync(CancellationToken token)
+    {
+        var vendors = await session.Query<CreateVendorResponse>().ToListAsync();
+
+        return Ok(vendors);
     }
 
 }
 
 /*{
-
     "name": "Microsoft",
-
     "url": "https://wwww.microsoft.com",
-
     "pointOfContact": {
-
         "name": "Satya",
-
         "phone": "800 big-boss",
-
         "email": "satya@microsoft.com"
-
     }
-
 }*/
 
 
-public record CreateVendorRequest(
+public record CreateVendorRequest
+{
+   
+    public string Name { get; init; } = string.Empty;
+    public string Url { get; init; } = string.Empty;
+    public CreateVendorPointOfContactRequest PointOfContact { get; init; } = new();
+}
 
-    string Name, string Url, CreateVendorPointOfContactRequest PointOfContact);
+public class CreateVendorRequestValidator : AbstractValidator<CreateVendorRequest>
+{
+    public CreateVendorRequestValidator()
+    {
+        RuleFor(v => v.Name).NotEmpty().MinimumLength(3).MaximumLength(255);
+        RuleFor(v => v.Url).NotEmpty();
+        RuleFor(v => v.PointOfContact).NotNull();
+    }
+}
 
-public record CreateVendorPointOfContactRequest(string Name, string Phone, string Email);
+public record CreateVendorPointOfContactRequest
+{
+    public string Name { get; init; } = string.Empty;
+    public string Phone { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+};
 
+public class CreateVendorPointOfContactRequestValidator :
+    AbstractValidator<CreateVendorPointOfContactRequest>
+{
+    public CreateVendorPointOfContactRequestValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty();
+ 
+    }
+}
 
 public record CreateVendorResponse(
-
     Guid Id,
-
     string Name, string Url, CreateVendorPointOfContactRequest PointOfContact
-
     );
