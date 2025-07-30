5d58bdf11b2d3ffd11467cd32701e0d5cc103546
Jose R Hernandez <jose@markojudas.com>
2025-07-30 18:24:21 +0000

    after lunch wednesday

---
+++ lab1/src/ShowsSolution/Shows.Api/Shows/Controller.cs
@@ -0,0 +1,58 @@
+﻿using Marten;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shows.Api.Shows
+{
+    public class Controller(IDocumentSession session): ControllerBase
+    {
+        [HttpPost("/api/shows")]
+        public async Task<ActionResult> AddAShowAsync(
+            [FromBody] AddShowRequest request)
+        {
+            var res = new AddShowResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+                StreamingService = request.StreamingService,
+                CreatedAt = DateTimeOffset.UtcNow
+            };
+            session.Store(res);
+            await session.SaveChangesAsync();
+            return Ok(res);
+        }
+
+        [HttpGet("/api/shows/{id:guid}")]
+        public async Task<ActionResult> GetShowById(Guid id)
+        {
+            var response = await session
+                .Query<AddShowResponse>()
+                .Where(session => session.Id == id)
+                .SingleOrDefaultAsync();
+
+            if (response == null) { 
+                return NotFound();
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }    
+    }
+
+    public record  AddShowRequest
+    {
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public string StreamingService { get; init; } = string.Empty;
+    }
+
+    public record AddShowResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public string StreamingService { get; init; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; init; }
+    }
+}
--- lab1/src/ShowsSolution/Shows.Tests/Api/Shows/AddingAShow.cs
+++ lab1/src/ShowsSolution/Shows.Tests/Api/Shows/AddingAShow.cs
@@ -1,4 +1,5 @@
 using Alba;
+using Shows.Api.Shows;
 using Shows.Tests.Api.Fixtures;
 
 namespace Shows.Tests.Api.Shows;
@@ -12,18 +13,31 @@ public class AddingAShow(SystemTestFixture fixture)
     [Fact]
     public async Task AddShow()
     {
+        var requestBody = new AddShowRequest
+        {
+            Name = "My Name Is Earl",
+            Description = "Lowlife turns life around thanks to Carson Daily",
+            StreamingService = "PlexStation"
+        };
         var response = await _host.Scenario(_ =>
         {
-            _.Post.Json(new
-            {
-                Name = "Test Show",
-                Description = "This is a test show",
-                StreamingService = "HBO Max"
-            }).ToUrl("/api/shows");
+            _.Post.Json(requestBody).ToUrl("/api/shows");
+        });
+
+        var postResponseBody = await response.ReadAsJsonAsync<AddShowResponse>();
+
+        Assert.NotNull(postResponseBody);
+
+        var getResponse = await _host.Scenario(_ =>
+        {
+            _.Get.Url($"/api/shows/{postResponseBody.Id}");
             _.StatusCodeShouldBeOk();
         });
-        
 
+        var getResponseBody = await getResponse.ReadAsJsonAsync<AddShowResponse>();
+
+        Assert.NotNull(getResponseBody);
+
+        Assert.Equal(postResponseBody, getResponseBody);
     }
-    
 }
